88ff52ab576b030df90e8c7e6d2e1236da162cb3
Hunter <hvanlear@gmail.com>
2023-11-16 09:56:49 -0500

    testing

--- src/server.ts
+++ src/server.ts
@@ -6,25 +6,28 @@ const app = express();
 
 const path = require("path");
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   // res.sendFile(path.resolve("src/pages/index.html"));
-  res.sendFile(__dirname+"/index.html");
+  res.sendFile(path.resolve("pages/index.html"));
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.use("/api", router);
 app.use((err, req, res, next) => {
-  if(err.type === 'auth'){
-    res.status(401).json({message:'unauthorized'})
-  } else if (err.type === 'input'){
-    res.status(400).json({message: 'bad request'})
+  if (err.type === "auth") {
+    res.status(401).json({ message: "unauthorized" });
+  } else if (err.type === "input") {
+    res.status(400).json({ message: "bad request" });
   } else {
-    res.status(500).json({message: 'All work and no play makes a server error'})
+    res
+      .status(500)
+      .json({ message: "All work and no play makes a server error" });
   }
-})
+});
 
 export default app;

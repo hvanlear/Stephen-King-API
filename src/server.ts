f00b075f2ea304641ffa8bfe29a493ea1e819960
Hunter <hvanlear@gmail.com>
2023-11-16 11:30:02 -0500

    #

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,8 @@ const app = express();
 
 const path = require("path");
 
-// app.use(express.static(__dirname + "/public"));
-app.use("/static", express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname + "/public"));
+// app.use("/static", express.static(path.join(__dirname, "public")));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
